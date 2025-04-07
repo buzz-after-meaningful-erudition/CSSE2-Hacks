df1b9e28722c78b91ab0adc9d40b0fb01cf54a5e
Ruhaan Bansal <ruhamobile@gmail.com>
2025-04-07 13:29:39 -0700

    change npc.js

--- assets/js/adventureGame/Npc.js
+++ assets/js/adventureGame/Npc.js
@@ -3,7 +3,7 @@ import Character from "./Character.js";
 class Npc extends Character {
     constructor(data = null, gameEnv = null) {
         super(data, gameEnv);
-        this.interact = data?.interact; // Interact function
+        this.interact = data?.interact ?? (() => {});
         this.currentQuestionIndex = 0;
         this.alertTimeout = null;
         this.bindInteractKeyListeners();
@@ -18,13 +18,13 @@ class Npc extends Character {
         addEventListener('keyup', this.handleKeyUp.bind(this));
     }
 
-    handleKeyDown({ key }) {
+    handleKeyDown = ({ key }) => {
         if (key === 'e' || key === 'u') {
             this.handleKeyInteract();
         }
     }
 
-    handleKeyUp({ key }) {
+    handleKeyUp = ({ key }) => {
         if (key === 'e' || key === 'u') {
             if (this.alertTimeout) {
                 clearTimeout(this.alertTimeout);
@@ -33,11 +33,17 @@ class Npc extends Character {
         }
     }
 
-    handleKeyInteract() {
-        const players = this.gameEnv.gameObjects.filter(
-            obj => obj.state.collisionEvents.includes(this.spriteData.id)
-        );
-        const hasInteract = this.interact !== undefined;
+    handleKeyInteract = () => {
+        const players = this.gameEnv?.gameObjects?.filter(
+            obj => obj?.state?.collisionEvents?.includes(this.spriteData.id)
+        ) || [];
+
+        const hasInteract = typeof this.interact === "function";
+
+        console.log("Interact key pressed");
+        console.log("NPC ID:", this.spriteData.id);
+        console.log("Players colliding:", players.map(p => p.spriteData?.id));
+        console.log("Interact function exists:", hasInteract);
 
         if (players.length > 0 && hasInteract) {
             this.interact();
